e220ae078ca4b0fad429c358d6f71f930fabc09c
BALAJI.J.B <31539453+BalajiJBcs@users.noreply.github.com>
2017-11-15 10:17:08 +0530

    Create multi_3_5.php

---
+++ multi_3_5.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by Balaji.J.B.
+ * Date: 15-11-2017
+ *
+ */
+
+$max_number =  1000;
+
+for ($i= 3; $i<$max_number; $i +=3 )
+{
+    $multiple_3 += $i;
+    echo  $i.',</br>';
+
+}
+echo '<br>';
+echo $multiple_3;
+echo '<br>';
+
+?>
